ff953449d78a82ca2b029429050e40af4b64d799
elakuladajefula <ela.wisniewska3@wp.pl>
2023-01-19 17:34:42 +0100

    change routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import Router from "vue-router";
 import MyShows from './pages/MyShows.vue'
 import MyFriends from './pages/MyFriends.vue'
 import SearchFriends from './pages/SearchFriends.vue'
@@ -6,56 +6,50 @@ import SearchShows from './pages/SearchShows.vue'
 import ProfileSettings from './pages/ProfileSettings.vue'
 import LogIn from './pages/LogIn.vue'
 import RegisterUser from './pages/RegisterUser.vue'
-import FriendShows from './pages/FriendShows.vue'
 
-const routes = 
-[
-    {
-        name: "LogIn",
-        path: "/",
-        component: LogIn,
-    },
-    {
-        name: "Register",
-        path: "/register",
-        component: RegisterUser,
-    },
-    {
-        name: "MyShows",
-        path: "/my-shows",
-        component: MyShows,
-    },
-    {
-        name: "MyFriends",
-        path: "/my-friends",
-        component: MyFriends,
-    },
-    {
-        name: "SearchShows",
-        path: "/search-shows",
-        component: SearchShows,
-    },
-    {
-        name: "SearchFriends",
-        path: "/search-friends",
-        component: SearchFriends,
-    },
-    // {
-    //     name: "FriendShows",
-    //     path: "/friend-shows",
-    //     component: FriendShows,
-    // },
-    {
-        name: "ProfileSettings",
-        path: "/profile-settings",
-        component: ProfileSettings,
-    },
-];
-
-const router = createRouter(
+Vue.use(Router);
+export default new Router(
 {
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-});
-
-export default router;
+    routes: 
+    [
+        {
+            name: "LogIn",
+            path: "/",
+            component: LogIn,
+        },
+        {
+            name: "Register",
+            path: "/register",
+            component: RegisterUser,
+        },
+        {
+            name: "MyShows",
+            path: "/my-shows",
+            component: MyShows,
+        },
+        {
+            name: "MyFriends",
+            path: "/my-friends",
+            component: MyFriends,
+        },
+        {
+            name: "SearchShows",
+            path: "/search-shows",
+            component: SearchShows,
+        },
+        {
+            name: "SearchFriends",
+            path: "/search-friends",
+            component: SearchFriends,
+        },
+        {
+            name: "ProfileSettings",
+            path: "/profile-settings",
+            component: ProfileSettings,
+        },
+        {
+            path: '*',
+            redirect: 'my-shows'
+        },
+    ]
+})
